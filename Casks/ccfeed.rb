f5580ffc37c0126dc71dddb31fae917df6b1e28d
Max Parke <max.parke@scale.com>
2026-05-29 17:41:42 -0400

    ccfeed 0.1.1

--- Casks/ccfeed.rb
+++ Casks/ccfeed.rb
@@ -1,6 +1,6 @@
 cask "ccfeed" do
-  version "0.1.0"
-  sha256 "fed7134aa7bb0eb7c2f9c86d7146b7f85d6d03ea0cbeb86a8fe51fdfe292b733"
+  version "0.1.1"
+  sha256 "67395c84096fe31fe5c3436ada3c5fc8ec4ddd0b3412776bec95af2b4c92b348"
 
   url "https://github.com/max-parke-scale/homebrew-tap/releases/download/v#{version}/ccfeed-#{version}.zip"
   name "ccfeed"
